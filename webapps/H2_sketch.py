8332cd4cb71ad2bb9cd0d582db69dc6b5dfc17c9
mariusholzinger <116267361+mariusholzinger@users.noreply.github.com>
2023-02-09 01:33:56 +0100

    Add files via upload

--- webapps/H2_sketch.py
+++ webapps/H2_sketch.py
@@ -5,6 +5,7 @@ from pyodide.ffi import create_proxy, to_js
 # Import python module
 import math
 
+
 #-----------------------------------------------------------------------
 # USE THIS FUNCTION TO WRITE THE MAIN PROGRAM
 def main():
@@ -21,10 +22,12 @@ def main():
 
     # Set up the scene
     scene = THREE.Scene.new()
-    back_color = THREE.Color.new(0.1,0.1,0.1)
+    back_color = THREE.Color.new(0.9,0.9,0.9)
     scene.background = back_color
-    camera = THREE.PerspectiveCamera.new(75, window.innerWidth/window.innerHeight, 0.1, 1000)
-    camera.position.z = 50
+    camera = THREE.PerspectiveCamera.new(75, window.innerWidth/window.innerHeight, 0.1, 3000)
+    camera.position.z = 800
+    camera.position.y = 1000
+    camera.position.x = 0
     scene.add(camera)
 
     # Graphic Post Processing
@@ -34,15 +37,31 @@ def main():
     # Set up responsive window
     resize_proxy = create_proxy(on_window_resize)
     window.addEventListener('resize', resize_proxy) 
+
+    global def_params
+    def_params = {
+
+        "size": 5,
+    }
+
+    def_params = Object.fromEntries(to_js(def_params))
     #-----------------------------------------------------------------------
     # YOUR DESIGN / GEOMETRY GENERATION
     # Geometry Creation
-    my_axiom_system = system(0, 3, "X")
+    my_axiom_system = system(0, def_params.size, "X")
 
     console.log(my_axiom_system)
 
     draw_system((my_axiom_system), THREE.Vector3.new(0,0,0))
 
+
+    my_axiom_system2 = system2(0, def_params.size, "X")
+
+    
+
+    console.log(my_axiom_system2)
+
+    draw_system2((my_axiom_system2), THREE.Vector3.new(0,0,0))
 
     #-----------------------------------------------------------------------
     # USER INTERFACE
@@ -50,11 +69,9 @@ def main():
     controls = THREE.OrbitControls.new(camera, renderer.domElement)
 
     # Set up GUI
-    """gui = window.dat.GUI.new()
+    """ gui = window.dat.GUI.new()
     param_folder = gui.addFolder('Parameters')
-    param_folder.add(geom1_params, 'size', 10,100,1)
-    param_folder.add(geom1_params, 'x', 2,100,1)
-    param_folder.add(geom1_params, 'rotation', 0,180)
+    param_folder.add(def_params, 'size', 2,10,1)
     param_folder.open()"""
     
     #-----------------------------------------------------------------------
@@ -78,6 +95,7 @@ def generate(symbol):
     elif symbol == "]":
         return "]"
 # A recursive fundtion, which taken an AXIOM as an inout and runs the generate function for each symbol
+
 def system(current_iteration, max_iterations, axiom):
     current_iteration += 1
     new_axiom = ""
@@ -134,93 +152,89 @@ def draw_system(axiom, start_pt):
         console.log(points)
 
         line_geom.setFromPoints( points )
-
-        material = THREE.LineBasicMaterial.new( THREE.Color.new(0x0000ff))
+        color = THREE.Color.new(255,0,0)
+        material = THREE.LineBasicMaterial.new(color)
+        material.color = color
         vis_line = THREE.Line.new( line_geom, material )
 
         
         global scene
         scene.add(vis_line)
 
+#-------------------------------------------------------------------------------------------------------------
 
-'''# FLIP---Define RULES in a function which takes one SYMBOL and applies rules generation
-def generate(fsymbol):
-    if fsymbol == "X":
-        return "FFF[-X]F[+X]-F[-X]F[+X]-X"
-    elif fsymbol == "F":
-        return "FF"
-    elif fsymbol == "+":
-        return "+"
-    elif fsymbol == "-":
-        return "-"
-    elif fsymbol == "[":
-        return "["
-    elif fsymbol == "]":
-        return "]"
-# A recursive fundtion, which taken an AXIOM as an inout and runs the generate function for each symbol
-def system(current_iterationf, max_iterationsf, axiomf):
-    current_iterationf += 1
-    new_axiomf = ""
-    for fsymbol in axiomf:
-        new_axiomf += generate(fsymbol)
-    if current_iterationf >= max_iterationsf:
-        return new_axiomf
+def system2(current_iteration, max_iterations, axiom):
+    current_iteration += 1
+    new_axiom = ""
+    for symbol in axiom:
+        new_axiom += generate(symbol)
+    if current_iteration >= max_iterations:
+        return new_axiom
     else:
-        return system(current_iterationf, max_iterationsf, new_axiomf)
+        return system2(current_iteration, max_iterations, new_axiom)
 
-def draw_system(axiomf, start_ptf):
-    move_vecf = THREE.Vector3.new(0,5,0)
-    old_statesf = []
-    old_move_vecsf = []
-    linesf = []
+def draw_system2(axiom, start_pt):
+    move_vec = THREE.Vector3.new(0,5,0)
+    old_states = []
+    old_move_vecs = []
+    lines = []
 
-    for fsymbol in axiomf:
-        if fsymbol == "F" or fsymbol == "X":
-            oldf = THREE.Vector3.new(start_ptf.x, start_ptf.y, start_ptf.z)
-            new_ptf = THREE.Vector3.new(start_ptf.x, start_ptf.y, start_ptf.z)
-            new_ptf = new_ptf.add(move_vecf)
-            linef = []
-            linef.append(oldf)
-            linef.append(new_ptf)
-            linesf.append(linef)
+    for symbol in axiom:
+        if symbol == "F" or symbol == "X":
+            old = THREE.Vector3.new(start_pt.x, start_pt.y, start_pt.z)
+            new_pt = THREE.Vector3.new(start_pt.x, start_pt.y, start_pt.z)
+            new_pt = new_pt.add(move_vec)
+            line = []
+            line.append(old)
+            line.append(new_pt)
+            lines.append(line)
 
-            start_ptf = new_ptf
+            start_pt = new_pt
 
-        elif fsymbol == "+": 
-            move_vecf.applyAxisAngle(THREE.Vector3.new(0,1,0), -math.pi/6)
-            move_vecf.applyAxisAngle(THREE.Vector3.new(0,1,-1), -math.pi/9)
+        elif symbol == "+": 
+            move_vec.applyAxisAngle(THREE.Vector3.new(0,1,0), -math.pi/6)
+            move_vec.applyAxisAngle(THREE.Vector3.new(0,1,-1), -math.pi/9)
 
-        elif fsymbol == "-":
-            move_vecf.applyAxisAngle(THREE.Vector3.new(1,0,1), math.pi/6)
+        elif symbol == "-":
+            move_vec.applyAxisAngle(THREE.Vector3.new(1,0,1), math.pi/6)
+           
+
         
-        elif fsymbol == "[":
-            old_statef = THREE.Vector3.new(start_ptf.x, start_ptf.y, start_ptf.z)
-            old_move_vecf = THREE.Vector3.new(move_vecf.x, move_vecf.y, move_vecf.z)
-            old_statesf.append(old_statef)
-            old_move_vecsf.append(old_move_vecf)
+        elif symbol == "[":
+            old_state = THREE.Vector3.new(start_pt.x, start_pt.y, start_pt.z)
+            old_move_vec = THREE.Vector3.new(move_vec.x, move_vec.y, move_vec.z)
+            old_states.append(old_state)
+            old_move_vecs.append(old_move_vec)
 
-        elif fsymbol == "]":
-            start_ptf = THREE.Vector3.new(old_statesf[-1].x, old_statesf[-1].y, old_statesf[-1].z)
-            move_vecf = THREE.Vector3.new(old_move_vecsf[-1].x, old_move_vecsf[-1].y, old_move_vecsf[-1].z)
-            old_statesf.pop(-1)
-            old_move_vecsf.pop(-1)
+        elif symbol == "]":
+            start_pt = THREE.Vector3.new(old_states[-1].x, old_states[-1].y, old_states[-1].z)
+            move_vec = THREE.Vector3.new(old_move_vecs[-1].x, old_move_vecs[-1].y, old_move_vecs[-1].z)
+            old_states.pop(-1)
+            old_move_vecs.pop(-1)
 
 
 
-    for fpoints in linesf:
-        fline_geom = THREE.BufferGeometry.new()
-        fpoints = to_js(fpoints)
+    for points in lines:
+        line_geom = THREE.BufferGeometry.new()
+        points = to_js(points)
         
-        console.log(fpoints)
-
-        fline_geom.setFromPoints( fpoints )
-
-        fmaterial = THREE.LineBasicMaterial.new( THREE.Color.new(0x0000ff))
-        fvis_line = THREE.Line.new( fline_geom, fmaterial )
+        console.log(points)
+        global vis_line
+        line_geom.setFromPoints( points )
+        color = THREE.Color.new(255,0,0)
+        material = THREE.LineBasicMaterial.new(color)
+        material.color = color
+        vis_line = THREE.Line.new( line_geom, material )
 
         
         global scene
-        scene.add(fvis_line)'''
+        scene.add(vis_line)
+
+def onSliderChange():
+    global max_iterations
+    max_iterations = def_params.size
+    render()
+    
 
 # Simple render and animate
 def render(*args):
